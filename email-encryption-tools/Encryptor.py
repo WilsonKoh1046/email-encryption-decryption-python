3f12770d4f6ec4aad7b37da01a0c0e315c621039
Wilson Koh <NikoKoh98@gmail.com>
2019-10-02 19:46:20 +0800

    Improving the encryptor

--- email-encryption-tools/Encryptor.py
+++ email-encryption-tools/Encryptor.py
@@ -3,13 +3,17 @@ def encryptor():
     # Lists of encrypting characters
     charList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
                 "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
-                "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", 
+                "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+                "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                 "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", 
                 "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",
             "~", "{", "}", "|", ":", "<", ">", "?", "-", "[", "]", ",",
-            ".", ";", ""]
+            ".", ";", " "]
     charListEncrypted = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+",
             "~", "{", "}", "|", ":", "<", ">", "?", "-", "=", "[", "]", ",",
-            ".", "..", ",,,,?", "///}{[", "^&%", "()&%^$:", "$%#$@#@!", ":(*&<)", "--++!", "@%^&$#$@!!", "?/)^%#@@",
+            ".", "!!", "@@", "##", "$$", "%:%", "^^", "&&", "**", "((", "))", "__", "++", "~~", "{:{",
+            "}:}", "||", "::", "<<", ">>", "??", "--", "==", "[[", "]]", ",,", "..",
+             "...", ",,,,?", "///}{[", "^&%", "()&%^$:", "$%#$@#@!", ":(*&<)", "--++!", "@%^&$#$@!!", "?/)^%#@@",
              ";", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
             "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
             "y", "z"]
@@ -19,23 +23,18 @@ def encryptor():
     
     result = ""
     for i in range(len(msg)):
-        if type(msg[i]) == str:
-            msg[i].lower()
-            if msg[i] in charList:
-                result += charListEncrypted[charList.index(msg[i])] # Encrypt input char by corresponding encrypting char 
-                i += 1
-            else:
-                i += 1
+        if msg[i] in charList:
+            result += charListEncrypted[charList.index(msg[i])] # Encrypt input char by corresponding encrypting char 
+            i += 1
         else:
-            if msg[i] in charList:
-                result += charListEncrypted[charList.index(msg[i])]
-                i += 1
-            else:
-                i += 1
+            i += 1
 
     print("\n" + result + "\n")
+    # To check the correctness of the char list
+    print(len(charList))
+    print(len(charListEncrypted))
 
-    choice = input("Do you want to decrypt the message?: Y/N")
+    choice = input("Do you want to decrypt the message?: Y/N ")
 
     if choice == "Y" or choice == "y":
         decrypted = ""
